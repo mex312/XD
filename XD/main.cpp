47fcc8d23d52868333dd99f54ccbbbccf9ae0c83
mex312 <mex3122020@gmail.com>
2022-11-25 13:48:45 +0300

    Added GLCamera class

--- XD/main.cpp
+++ XD/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <cmath>
 #include "GLShader.h"
+#include "GLCamera.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <SOIL/SOIL.h>
@@ -11,10 +12,45 @@
 
 GLFWwindow* window;
 
+GLfloat sensitivity = 0.2f;
+
+bool keys[1024] = { false };
+
+GLfloat deltaTime;
+
+GLfloat lastX = 400, lastY = 300;
+
+GLCamera camera;
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (action == GLFW_PRESS) {
+		keys[key] = true;
+	}
+	else if (action == GLFW_RELEASE) {
+		keys[key] = false;
+	}
+}
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+	camera.ProcessMouseScroll(yoffset);
 }
 
 
@@ -42,13 +78,30 @@ int init() {
 	}
 
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 
 	glViewport(0, 0, width, height);
 
+	glEnable(GL_DEPTH_TEST);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	return 0;
+}
+
+
+void handleMovement() {
+	if (keys[GLFW_KEY_W])
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 
@@ -59,13 +112,50 @@ int main() {
 
 
 	GLfloat vertices[] = {
-	//  x      y      z        r      g      b      a        tx     ty
-		 0.5f,  0.5f,  0.0f,    1.0f,  0.0f,  0.0f,  1.0f,    1.0f,  1.0f,  // Top Right
-		 0.5f, -0.5f,  0.0f,    0.0f,  1.0f,  0.0f,  1.0f,    1.0f,  0.0f,  // Bottom Right
-		-0.5f, -0.5f,  0.0f,    0.0f,  0.0f,  1.0f,  1.0f,    0.0f,  0.0f,  // Bottom Left
-		-0.5f,  0.5f,  0.0f,    0.0f,  0.0f,  0.0f,  1.0f,    0.0f,  1.0f,  // Top Left 
+	//  x      y      z      tx    ty
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	GLuint indices[] = {  // Note that we start from 0!
+	GLuint indices[] = {
 		0, 1, 3,  // First Triangle
 		1, 2, 3   // Second Triangle
 	};
@@ -84,14 +174,14 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(7 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
+	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(7 * sizeof(GLfloat)));
+	//glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
@@ -113,39 +203,50 @@ int main() {
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glm::mat4 mat(1);
-	mat = glm::rotate(mat, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
+	glm::mat4 projection(1);
+	glm::mat4 view(1);
+	glm::mat4 model(1);
+	model = glm::rotate(model, glm::radians(-60.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	GLfloat lastTime = glfwGetTime();
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
+		handleMovement();
 
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		GLfloat time = glfwGetTime();
-		GLfloat green = (sin(time) / 2) + 0.5f;
+		deltaTime = time - lastTime;
 		GLuint customColor = glGetUniformLocation(shader.program, "customColor");
-		GLuint transform = glGetUniformLocation(shader.program, "transform");
+		GLuint u_projection = glGetUniformLocation(shader.program, "u_projection");
+		GLuint u_view= glGetUniformLocation(shader.program, "u_view");
+		GLuint u_model = glGetUniformLocation(shader.program, "u_model");
+
+		projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
+		view = camera.GetViewMatrix();
 
 		// Draw our first triangle
 		shader.use();
 
-		glUniform4f(customColor, 0, green, 0, 1);
-		glUniformMatrix4fv(transform, 1, GL_FALSE, glm::value_ptr(mat));
+		glUniform4f(customColor, 0, 0, 0, 1);
+		glUniformMatrix4fv(u_projection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(u_view, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(u_model, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glBindTexture(GL_TEXTURE_2D, texture);
 
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
+		lastTime = time;
 	}
 
 	glDeleteVertexArrays(1, &VAO);
--- XD/shader.vert
+++ XD/shader.vert
@@ -1,10 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec3 raw_position;
-layout (location = 1) in vec4 raw_color;
-layout (location = 2) in vec2 raw_texCoord;
+layout (location = 1) in vec2 raw_texCoord;
+//layout (location = 2) in vec4 raw_color;
 
-uniform mat4 transform;
+uniform mat4 u_projection;
+uniform mat4 u_view;
+uniform mat4 u_model;
+
+
 
 out vec4 vertexColor;
 out vec2 texCoord;
@@ -12,7 +16,8 @@ out vec2 texCoord;
 void main()
 {
 	//gl_Position = transform * vec4(raw_position, 1.0f);
-	gl_Position = transform * vec4(raw_position, 1.0f);
-	vertexColor = raw_color;
+	gl_Position = u_projection * u_view * u_model * vec4(raw_position, 1.0f);
+	//vertexColor = raw_color;
+	vertexColor = vec4(1, 1, 1, 1);
 	texCoord = raw_texCoord;
 }
